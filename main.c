eb5bca59209dcf21d09a961bd3593b6879ebe9e8
Jacob Preston <prestja@gmail.com>
2021-09-20 17:31:33 -0500

    Additional comments

--- main.c
+++ main.c
@@ -5,15 +5,21 @@
 
 void main()
 {
-    int frameCount = 1;
+    int frameCount = 0;
     while(1)
     {
+        // game logic
         if (frameCount % FRAME_INCREMENT == 0)
         {
-            printf("Animate\n", frameCount);
+            printf("anim on frame %d\n", frameCount);
+        }
+
+        // frame increment and timing
+        frameCount++;
+        if (frameCount >= 60)
+        {
             frameCount = 0;
         }
-        frameCount++;
         wait_vbl_done();
     }
 }
