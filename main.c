6fc3bd6342c606663a65a3db6078382de5a0fc2d
Jacob Preston <prestja@gmail.com>
2021-09-20 17:17:03 -0500

    Adds simple frame counter and output

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <gb/gb.h>
+#include <gb/types.h>
 #include <stdio.h>
 
 void main()
 {
-    printf("HELLO WORLD");
+    BYTE frameCount = 0;
+    while(1)
+    {
+        printf("HELLO WORLD %d\n", frameCount);
+        frameCount++;
+        wait_vbl_done();
+    }
 }
