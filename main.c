e587ce1daa136180dc67db6527b5830ae80952dc
Jeremy Hahn <annony3156@gmail.com>
2021-10-04 20:19:57 -0400

    added pot array

--- main.c
+++ main.c
@@ -6,39 +6,62 @@
 #include "pots.c"
 #include "meta_sprite.c"
 
-struct metaSprite potting;
+movingPot potting[8];
+UINT8 size = 16;
 
-spriteMovment(struct metaSprite* sp, UINT8 x, UINT8 y){
+void spriteMovment(metaSprite* sp, UINT8 x, UINT8 y){
     move_sprite(sp->ID[0], x, y);
     move_sprite(sp->ID[1], x + 8, y);
     move_sprite(sp->ID[2], x, y + 8);
     move_sprite(sp->ID[3], x + 8, y + 8);
 }
 
-void setupPot(){
-    potting.x = 20;
-    potting.y = 17;
-    potting.w = 16;
-    potting.h = 16;
+/*
+void updatePot(metaSprite* pot){
 
-    set_sprite_tile(0,0);
-    potting.ID[0] = 0;
-    set_sprite_tile(1,1);
-    potting.ID[1] = 1;
-    set_sprite_tile(2,2);
-    potting.ID[2] = 2;
-    set_sprite_tile(3,3);
-    potting.ID[3] = 3;
+}
 
-    spriteMovment(&potting, potting.x, potting.y);
+void updatePlayer(metaSprite* player){
+
+}
+*/
+void setupPot(UINT8 x, UINT8 y, UINT8 potID){
+    
+    UINT8 spriteID = potID >> 2;
+    //struct movingPot pot = ms -> data;
+    potting[potID].sprite.x = x;
+    potting[potID].y = y;
+    potting[potID].sprite.w = size;
+    potting[potID].sprite.h = size;
+
+    set_sprite_tile(spriteID,0);
+    potting[potID].sprite.ID[0] = 0;
+    set_sprite_tile(spriteID + 1,1);
+    potting[potID].sprite.ID[1] = 1;
+    set_sprite_tile(spriteID + 2,2);
+    potting[potID].sprite.ID[2] = 2;
+    set_sprite_tile(spriteID + 3,3);
+    potting[potID].sprite.ID[3] = 3;
+
+    spriteMovment(&potting, potting[potID].sprite.x, potting[potID].y);
 }
 
 //setup sprites, widow, and background
 void init(){
+    UINT8 counter = 0;
+    UINT8 x = 20;
+    UINT8 y = 17;
     set_sprite_data(0,4, pots);
+    
+    while (counter < 8){
+        setupPot(x,y,counter);
+        x -= 20;
+        y -= 18;
+        counter ++;
+    }
     set_bkg_data(0, 3, shelf);
     set_bkg_tiles(0,0,20,18,bkg_map);
-    setupPot();
+    
 
     SHOW_SPRITES;
     SHOW_BKG;
--- meta_sprite.c
+++ meta_sprite.c
@@ -1,9 +1,19 @@
 #include <gb/gb.h>
-
-struct metaSprite{
+typedef struct{
     UINT8 ID[4];
     UINT8 x;
-    UINT8 y;
     UINT8 h;
     UINT8 w;
-};
+} metaSprite;
+
+typedef struct{
+    metaSprite sprite;
+    UBYTE placeholder;
+} player;
+
+typedef struct
+{
+    metaSprite sprite;
+    UINT8 y;
+} movingPot;
+
